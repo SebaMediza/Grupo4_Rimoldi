6c621c40eb6abb33275de3b569e03c4f77b289ab
Lauree13 <laureanopagola@gmail.com>
2024-09-13 09:25:43 -0300

    No se si esta bien esto muchachos, soy nuevito en esto 🦔

--- src/main/java/com/rimoldi/Main.java
+++ src/main/java/com/rimoldi/Main.java
@@ -4,8 +4,9 @@ import static spark.Spark.*;
 import com.rimoldi.practico1.controller.PracticoController;
 
 public class Main {
+    
     public static void main(String[] args) {
         PracticoController PracticoController = new PracticoController();
-        get("/cotizacion/:monto", PracticoController.getCotizacion);
+        get("/celsiusAfahrenheit/:celsius", PracticoController.getFahrenheit);
     }
 }
