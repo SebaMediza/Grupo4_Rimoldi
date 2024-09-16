27c1caed8a2e6a10730f1c2bab41cf87df824896
Baltasar Moreno <baltamoreno444@gmail.com>
2024-09-16 09:14:34 -0300

    ej2-nroPar

--- src/main/java/com/rimoldi/practico1/controller/PracticoController.java
+++ src/main/java/com/rimoldi/practico1/controller/PracticoController.java
@@ -1,28 +1,24 @@
 package com.rimoldi.practico1.controller;
 
-import com.rimoldi.practico1.model.DolarApi;
+import com.rimoldi.practico1.model.NumeroPrimo;
+
 import spark.*;
-import com.google.gson.Gson;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.URI;
 
 public class PracticoController {
+    public Route getNumero=(Request request, Response response) -> {
+        String nroParam = request.params(":numero");
 
-    private HttpClient client = HttpClient.newHttpClient();
-    private Gson gson = new Gson();
+        try {
+            int numero = Integer.parseInt(nroParam);
+            boolean primo = NumeroPrimo.esPrimo(numero);
 
-    public Route getCotizacion = (Request req, Response res) -> {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://dolarapi.com/v1/dolares/oficial"))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() != 200) {
-            throw new RuntimeException("Failed to get cotizacion" + response.statusCode());
+            if(primo==true){
+                return "El número "+numero+" es primo.";
+            }
+            return "El número "+numero+" NO es primo.";
+        } catch (NumberFormatException e) {
+            response.status(400);
+            return "Error: El parámetro 'numero' debe ser un número.";
         }
-        DolarApi dolarApi = gson.fromJson(response.body(), DolarApi.class);
-
-        return "Los " + req.params(":monto") + " dolares son " + (Integer.parseInt(req.params(":monto")) * Integer.parseInt(dolarApi.getCompra())) + " pesos.";
     };
 }
