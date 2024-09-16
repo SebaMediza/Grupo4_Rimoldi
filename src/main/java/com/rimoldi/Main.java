c8933165eb2eaf922205030d05f03f7e98d21c5f
Baltasar Moreno <baltamoreno444@gmail.com>
2024-09-16 15:44:01 -0300

    ej6-abmUsuarios

--- src/main/java/com/rimoldi/Main.java
+++ src/main/java/com/rimoldi/Main.java
@@ -6,6 +6,11 @@ import com.rimoldi.practico1.controller.PracticoController;
 public class Main {
     public static void main(String[] args) {
         PracticoController PracticoController = new PracticoController();
+        PracticoController PracticoController2=new PracticoController();
         get("/cotizacion/:monto", PracticoController.getCotizacion);
+        get("/usuarios", PracticoController2.getUsuarios);
+        post("/usuarios", PracticoController2.altaUsuario);
+        delete("/usuarios/:id", PracticoController2.eliminarUsuario);
+        put("/usuarios/:id", PracticoController2.modificarUsuario);
     }
 }
--- src/main/java/com/rimoldi/practico1/controller/PracticoController.java
+++ src/main/java/com/rimoldi/practico1/controller/PracticoController.java
@@ -1,8 +1,13 @@
 package com.rimoldi.practico1.controller;
 
 import com.rimoldi.practico1.model.DolarApi;
+import com.rimoldi.practico1.model.Usuario;
+import com.rimoldi.practico1.model.UsuarioABM;
+
 import spark.*;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -25,4 +30,46 @@ public class PracticoController {
 
         return "Los " + req.params(":monto") + " dolares son " + (Integer.parseInt(req.params(":monto")) * Integer.parseInt(dolarApi.getCompra())) + " pesos.";
     };
+    
+    public Route getUsuarios=(Request req, Response res) -> {
+        res.type("application/json");
+        return gson.toJson(UsuarioABM.getUsuarios());
+    };
+
+    public Route altaUsuario=(Request req, Response res) -> {
+        JsonObject json = gson.fromJson(req.body(), JsonObject.class);
+        String nombre = json.get("nombre").getAsString();
+        String email = json.get("email").getAsString();
+        Usuario usuario = UsuarioABM.crearUsuario(nombre, email);
+        res.status(201);
+        return gson.toJson(usuario);
+    };
+
+    public Route eliminarUsuario=(Request req, Response res) -> {
+        int id = Integer.parseInt(req.params(":id"));
+        Usuario usuario = UsuarioABM.eliminarUsuario(id);
+        if (usuario != null) {
+            res.status(200);
+            return gson.toJson(usuario);
+        } else {
+            res.status(404);
+            return "{\"error\": \"Usuario no encontrado.\"}";
+        }
+    };
+
+    public Route modificarUsuario=(Request req, Response res) -> {
+        int id = Integer.parseInt(req.params(":id"));
+        JsonObject json = gson.fromJson(req.body(), JsonObject.class);
+        String nombre = json.get("nombre").getAsString();
+        String email = json.get("email").getAsString();
+        Usuario usuario = UsuarioABM.modificarUsuario(id, nombre, email);
+        if (usuario != null) {
+            res.status(200);
+            return gson.toJson(usuario);
+        } else {
+            res.status(404);
+            return "{\"error\": \"Usuario no encontrado.\"}";
+        }
+    };
+
 }
